f7d59e75b17f44c4a4c12ba0f02e0010d5a966a6
Daniel Francisco Campos Pérez <dan.campos@estudiantec.cr>
2023-10-15 18:08:53 -0600

    Archivo en avance 1

---
+++ Semana 1/TareaProgramada2.py
@@ -0,0 +1,55 @@
+#Creado por: Daniel Campos y Alejandro Madrigal
+#Creación: 10-10-2023 10:00 pm
+#Ultima modificación: 12-10-2023 9:00 am
+#Versión: 3.12.0
+#Importacion de librerias
+import tkinter as tk
+#Definicion de funciones
+def estudiantesSede():
+    return
+
+def estudiantesCarrera():
+    return
+
+def crearMentores():
+    return
+
+def asignarMentores():
+    return
+
+def actualizarEstudiante():
+    return
+
+def generarReportes():
+    return
+
+def crearBaseDatos():
+    return
+
+def enviarCorreo():
+    return
+#Interfaz gráfica
+ventana = tk.Tk()
+ventana.title("Atención a la Generación 2024")
+ventana.attributes('-fullscreen', True)
+boton1 = tk.Button(ventana, text="Estudiantes por sede", command=estudiantesSede)
+boton2 = tk.Button(ventana, text="Estudiantes de carrera por sede", command=estudiantesCarrera, state="disabled")
+boton3 = tk.Button(ventana, text="Crear mentores", command=crearMentores, state="disabled")
+boton4 = tk.Button(ventana, text="Asignar mentores", command=asignarMentores, state="disabled")
+boton5 = tk.Button(ventana, text="Actualizar estudiante", command=actualizarEstudiante, state="disabled")
+boton6 = tk.Button(ventana, text="Generar reportes", command=generarReportes)
+boton7 = tk.Button(ventana, text="Crear base de datos en Excel", command=crearBaseDatos)
+boton8 = tk.Button(ventana, text="Enviar correo", command=enviarCorreo)
+boton9 = tk.Button(ventana, text="Salir", command=ventana.quit)
+
+boton1.pack()
+boton2.pack()
+boton3.pack()
+boton4.pack()
+boton5.pack()
+boton6.pack()
+boton7.pack()
+boton8.pack()
+boton9.pack()
+
+ventana.mainloop()
