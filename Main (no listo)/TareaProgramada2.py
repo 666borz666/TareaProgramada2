39ca2029165c3c711f75d86bcf7647665234ffeb
Daniel Francisco Campos Pérez <dan.campos@estudiantec.cr>
2023-10-22 19:27:58 -0600

    Arreglar tabla de info

--- Main (no listo)/TareaProgramada2.py
+++ Main (no listo)/TareaProgramada2.py
@@ -2,90 +2,93 @@
 #Creación: 10-10-2023 
 #Ultima modificación: 15-10-2023 
 #Versión: 3.12.0
+
 #Importacion de librerias
 import tkinter as tk
-import random
 from tkinter import ttk
+from tkinter import messagebox
+import random
 from faker import Faker
 import pandas as pd
-import os
-from tkinter import messagebox
-import os
+
 #Variables globales
 fake = Faker()
 cantidadEstudiantes = None
-print(os.getcwd())
-#Funciones
 excel = r'C:\Users\ADIC\Documents\GitHub\TareaProgramada2\Main (no listo)\sedes.xlsx'
 carreras = pd.read_excel(excel)
 asignacionesEstudiantes = []
 cantidadEstudiantes = None 
+
+#Funciones
 def estudiantesSede():
-    def asignarEstudiantes():
-        global asignacionesEstudiantes 
-        totalEstudiantes = int(cantidadEstudiantes_entry.get())
-        sede = sedeSeleccionada.get()
-        sedeCarreras = carreras[sede].dropna().index.tolist()
-        estudiantesAsignados = {carrera: 0 for carrera in sedeCarreras}
-        estudiantesRestantes = totalEstudiantes
-        while estudiantesRestantes > 0:
-            carreraAleatoria = random.choice(sedeCarreras)
-            estudiantesAsignados[carreraAleatoria] += 1
-            estudiantesRestantes -= 1
-        asignacionesEstudiantes = asignacionesEstudiantes + [{'carrera': carrera,'cantidadEstudiantes': cantidadEstudiantes} for carrera, cantidadEstudiantes in estudiantesAsignados.items()]
-        ventanaResultados = tk.Toplevel(ventana)
-        ventanaResultados.title("Resultados de Asignación de Estudiantes")
-        for carrera, estudiantesAsignados in estudiantesAsignados.items():
-            nombreCarrera = carreras.loc[carrera, sede]
-            labelResultado = tk.Label(ventanaResultados, text=f"{nombreCarrera}: {estudiantesAsignados} estudiantes asignados")
-            labelResultado.pack()
+    global asignacionesEstudiantes
     ventanaEstudiantes = tk.Toplevel(ventana)
     ventanaEstudiantes.title("Estudiantes por Sede")
     etiquetaSede = tk.Label(ventanaEstudiantes, text="Selecciona una sede:")
     etiquetaSede.pack()
-    opcionesSedes = ["CAMPUS TECNOLÓGICO LOCAL SAN CARLOS","CAMPUS TECNOLÓGICO LOCAL SAN JOSÉ","CENTRO ACADÉMICO DE LIMÓN","CAMPUS TECNOLÓGICO CENTRAL CARTAGO","CENTRO ACADÉMICO DE ALAJUELA"]
+    opcionesSedes = ["CAMPUS TECNOLÓGICO LOCAL SAN CARLOS", "CAMPUS TECNOLÓGICO LOCAL SAN JOSÉ","CENTRO ACADÉMICO DE LIMÓN", "CAMPUS TECNOLÓGICO CENTRAL CARTAGO","CENTRO ACADÉMICO DE ALAJUELA"]
     sedeSeleccionada = tk.StringVar()
     sedeSeleccionada.set(opcionesSedes[0])
     listaSede = ttk.Combobox(ventanaEstudiantes, textvariable=sedeSeleccionada, values=opcionesSedes)
+    listaSede.config(width=42)
     listaSede.pack()
     etiquetaCantidad = tk.Label(ventanaEstudiantes, text="Cantidad total de estudiantes:")
     etiquetaCantidad.pack()
     cantidadEstudiantes_entry = tk.Entry(ventanaEstudiantes)
     cantidadEstudiantes_entry.pack()
+    def asignarEstudiantes():
+        sede = sedeSeleccionada.get()
+        cantidad_estudiantes = int(cantidadEstudiantes_entry.get())
+        sedeCarreras = carreras[sede].dropna().index.tolist()
+        estudiantesAsignados = {carrera: 0 for carrera in sedeCarreras}
+        estudiantesRestantes = cantidad_estudiantes
+        while estudiantesRestantes > 0:
+            carreraAleatoria = random.choice(sedeCarreras)
+            estudiantesAsignados[carreraAleatoria] += 1
+            estudiantesRestantes -= 1
+        asignacion_sede = {'sede': sede, 'asignaciones': estudiantesAsignados}
+        asignacionesEstudiantes.append(asignacion_sede)
+        ventanaResultados = tk.Toplevel(ventana)
+        ventanaResultados.title("Resultados de Asignación de Estudiantes")
+        for carrera, cantidad_estudiantes in estudiantesAsignados.items():
+            nombreCarrera = carreras.loc[carrera, sede]
+            labelResultado = tk.Label(ventanaResultados, text=f"{nombreCarrera}: {cantidad_estudiantes} estudiantes asignados")
+            labelResultado.pack()
     botonAsignarEstudiantes = tk.Button(ventanaEstudiantes, text="Asignar Estudiantes", command=asignarEstudiantes)
     botonAsignarEstudiantes.pack()
-excel = r'C:\Users\ADIC\Documents\GitHub\TareaProgramada2\Main (no listo)\sedes.xlsx'
-carreras = pd.read_excel(excel)
+    botonCerrar = tk.Button(ventanaEstudiantes, text="Cerrar", command=ventanaEstudiantes.destroy)
+    botonCerrar.pack()
 
 def estudiantesCarrera():
-    def mostrarEstudiantesSede():
+    def mostrar_estudiantes_de_sede():
         sedeElegida = sedeSeleccionada.get()
         if sedeElegida in carreras:
             sedeCarreras = carreras[sedeElegida].dropna().index.tolist()
-            nombreCarrera = random.choice(sedeCarreras)
             ventanaResultados = tk.Toplevel(ventana)
             ventanaResultados.title("Estudiantes de la Sede")
             tabla = ttk.Treeview(ventanaResultados, columns=["Carnet", "Nombre Completo", "Carrera", "Teléfono", "Correo Electrónico", "Carnet de Mentor"])
-            tabla.heading("#1", text="Carnet")
-            tabla.heading("#2", text="Nombre Completo")
-            tabla.heading("#3", text="Carrera")
-            tabla.heading("#4", text="Teléfono")
-            tabla.heading("#5", text="Correo Electrónico")
-            tabla.heading("#6", text="Carnet de Mentor")
+            tabla.heading("#0", text="Carnet")
+            tabla.heading("#1", text="Nombre Completo")
+            tabla.heading("#2", text="Carrera")
+            tabla.heading("#3", text="Teléfono")
+            tabla.heading("#4", text="Correo Electrónico")
+            tabla.heading("#5", text="Carnet de Mentor")
             tabla.pack()
-            admitidosCarrera = cantidadAdmitidos(sedeElegida, nombreCarrera)
+            admitidosCarreras = {carrera: cantidadAdmitidos(sedeElegida, carrera) for carrera in sedeCarreras}
             estudiantesGenerados = []
             telefonosGenerados = set()
-            for _ in range(admitidosCarrera):
-                carnet = f"2024{opcionesSedes.index(sedeElegida) + 1:02d}{random.randint(1000, 9999)}"
-                nombreCompleto = (fake.last_name(), fake.last_name(), fake.first_name())
-                telefono = generarTelefono(telefonosGenerados)
-                correo = generarCorreo(nombreCompleto[2], nombreCompleto[0])
-                estudiantesGenerados.append({'Carnet': carnet,'Nombre Completo': f"{nombreCompleto[0]} {nombreCompleto[1]} {nombreCompleto[2]}",'Carrera': nombreCarrera,'Teléfono': telefono,'Correo Electrónico': correo,'Carnet de Mentor': "0",})
+            for carrera in sedeCarreras:
+                nombreCarrera = carreras.loc[carrera, sedeElegida]
+                for _ in range(admitidosCarreras[carrera]):
+                    carnet = f"2024{opcionesSedes.index(sedeElegida) + 1:02d}{random.randint(1000, 9999)}"
+                    nombreCompleto = (fake.last_name(), fake.last_name(), fake.first_name())
+                    telefono = generarTelefono(telefonosGenerados)
+                    correo = generar_correo(nombreCompleto[2], nombreCompleto[0])
+                    estudiantesGenerados.append({'Carnet': carnet, 'Nombre Completo': f"{nombreCompleto[0]} {nombreCompleto[1]} {nombreCompleto[2]}", 'Carrera': nombreCarrera, 'Teléfono': telefono, 'Correo Electrónico': correo, 'Carnet de Mentor': "0"})
             for estudiante in estudiantesGenerados:
                 tabla.insert("", "end", values=(estudiante['Carnet'], estudiante['Nombre Completo'], estudiante['Carrera'], estudiante['Teléfono'], estudiante['Correo Electrónico'], estudiante['Carnet de Mentor']))
         else:
-            messagebox.showerror("Error", f"La sede '{sedeElegida}' no se encuentra en el archivo Excel.")
+            messagebox.showerror("Error", f"La sede '{sedeElegida}' no se encuentra en el archivo Excel.")        
     ventanaSede = tk.Toplevel(ventana)
     ventanaSede.title("Seleccionar Sede")
     etiquetaSeleccionSede = tk.Label(ventanaSede, text="Selecciona una sede:")
@@ -94,8 +97,9 @@ def estudiantesCarrera():
     sedeSeleccionada = tk.StringVar()
     sedeSeleccionada.set(opcionesSedes[0])
     listaSede = ttk.Combobox(ventanaSede, textvariable=sedeSeleccionada, values=opcionesSedes)
+    listaSede.config(width=42)
     listaSede.pack()
-    botonConfirmarSede = tk.Button(ventanaSede, text="Mostrar Estudiantes", command=mostrarEstudiantesSede)
+    botonConfirmarSede = tk.Button(ventanaSede, text="Mostrar Estudiantes", command=mostrar_estudiantes_de_sede)
     botonConfirmarSede.pack()
     
 def cantidadAdmitidos(sede, carrera):
@@ -115,9 +119,9 @@ def generarTelefono(telefonosGenerados):
             telefonosGenerados.add(telefono)
             return telefono
 
-def generarCorreo(nombre, apellido):
+def generar_correo(nombre, apellido):
     correo = nombre[:2] + apellido.split()[0] + "@estudiantec.cr"
-    return correo
+    return correo.lower()
 
 def crearMentores():
     return
@@ -162,4 +166,3 @@ boton8.pack()
 boton9.pack()
 
 ventana.mainloop()
-
