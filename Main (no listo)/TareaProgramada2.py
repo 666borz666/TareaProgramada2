22a2539c49ee9c1211e263616468767bc130766e
MigaleMR <alemarra98@gmail.com>
2023-10-23 19:57:02 -0600

    Genera los mentores pero no la cantidad de mentores necesarios por carrera
    Intentarlo hacer con un 5% de mentores por carrera, o distribuir el total de 5% a cada carrera. Se había añadido a la función crearMentores que hiciera un binario, pero fue eliminado, y se olvidó añadirlo

--- Main (no listo)/TareaProgramada2.py
+++ Main (no listo)/TareaProgramada2.py
@@ -10,7 +10,7 @@ from tkinter import messagebox
 import random
 from faker import Faker
 import pandas as pd
-
+import pickle
 #Variables globales
 fake = Faker()
 cantidadEstudiantes = None
@@ -18,7 +18,7 @@ excel = r'D:\Estudios de Ale\Compu\GitHub\Tareas Programadas\TareaProgramada2\Ta
 carreras = pd.read_excel(excel)
 asignacionesEstudiantes = []
 cantidadEstudiantes = None 
-
+cantidad_estudiantes = 0
 #Funciones
 def estudiantesSede():
     global asignacionesEstudiantes
@@ -58,9 +58,8 @@ def estudiantesSede():
     botonAsignarEstudiantes.pack()
     botonCerrar = tk.Button(ventanaEstudiantes, text="Cerrar", command=ventanaEstudiantes.destroy)
     botonCerrar.pack()
-
 def estudiantesCarrera():
-    def mostrar_estudiantes_de_sede():
+    def mostrarSede():
         sedeElegida = sedeSeleccionada.get()
         if sedeElegida in carreras:
             sedeCarreras = carreras[sedeElegida].dropna().index.tolist()
@@ -83,25 +82,26 @@ def estudiantesCarrera():
                     carnet = f"2024{opcionesSedes.index(sedeElegida) + 1:02d}{random.randint(1000, 9999)}"
                     nombreCompleto = (fake.last_name(), fake.last_name(), fake.first_name())
                     telefono = generarTelefono(telefonosGenerados)
-                    correo = generar_correo(nombreCompleto[2], nombreCompleto[0])
+                    correo = generarCorreo(nombreCompleto[2], nombreCompleto[0])
                     estudiantesGenerados.append({'Carnet': carnet, 'Nombre Completo': f"{nombreCompleto[0]} {nombreCompleto[1]} {nombreCompleto[2]}", 'Carrera': nombreCarrera, 'Teléfono': telefono, 'Correo Electrónico': correo, 'Carnet de Mentor': "0"})
-            for estudiante in estudiantesGenerados:
-                tabla.insert("", "end", values=(estudiante['Carnet'], estudiante['Nombre Completo'], estudiante['Carrera'], estudiante['Teléfono'], estudiante['Correo Electrónico'], estudiante['Carnet de Mentor']))
+                    tabla.insert("", "end", values=(carnet, f"{nombreCompleto[0]} {nombreCompleto[1]} {nombreCompleto[2]}", nombreCarrera, telefono, correo, "0"))
+            return estudiantesGenerados  # Devuelve la lista de estudiantes generados
         else:
-            messagebox.showerror("Error", f"La sede '{sedeElegida}' no se encuentra en el archivo Excel.")        
+            messagebox.showerror("Error", f"La sede '{sedeElegida}' no se encuentra en el archivo Excel.")
+
     ventanaSede = tk.Toplevel(ventana)
     ventanaSede.title("Seleccionar Sede")
     etiquetaSeleccionSede = tk.Label(ventanaSede, text="Selecciona una sede:")
     etiquetaSeleccionSede.pack()
-    opcionesSedes = ["CAMPUS TECNOLÓGICO LOCAL SAN CARLOS","CAMPUS TECNOLÓGICO LOCAL SAN JOSÉ","CENTRO ACADÉMICO DE LIMÓN","CAMPUS TECNOLÓGICO CENTRAL CARTAGO","CENTRO ACADÉMICO DE ALAJUELA"]
+    opcionesSedes = ["CAMPUS TECNOLÓGICO LOCAL SAN CARLOS", "CAMPUS TECNOLÓGICO LOCAL SAN JOSÉ", "CENTRO ACADÉMICO DE LIMÓN", "CAMPUS TECNOLÓGICO CENTRAL CARTAGO", "CENTRO ACADÉMICO DE ALAJUELA"]
     sedeSeleccionada = tk.StringVar()
     sedeSeleccionada.set(opcionesSedes[0])
     listaSede = ttk.Combobox(ventanaSede, textvariable=sedeSeleccionada, values=opcionesSedes)
     listaSede.config(width=42)
     listaSede.pack()
-    botonConfirmarSede = tk.Button(ventanaSede, text="Mostrar Estudiantes", command=mostrar_estudiantes_de_sede)
+    botonConfirmarSede = tk.Button(ventanaSede, text="Mostrar Estudiantes", command=mostrarSede)
     botonConfirmarSede.pack()
-    
+#Auxiliares para estudiantesCarrera   
 def cantidadAdmitidos(sede, carrera):
     infoSede = [info for info in asignacionesEstudiantes if info['sede'] == sede]
     if infoSede:
@@ -119,16 +119,74 @@ def generarTelefono(telefonosGenerados):
             telefonosGenerados.add(telefono)
             return telefono
 
-def generar_correo(nombre, apellido):
+def generarCorreo(nombre, apellido):
     correo = nombre[:2] + apellido.split()[0] + "@estudiantec.cr"
     return correo.lower()
-
+#Realizar
 def crearMentores():
-    return
+    def mostrarSede():
+        sedeElegida = sedeSeleccionada.get()
+        if sedeElegida in carreras:
+            sedeCarreras = carreras[sedeElegida].dropna().index.tolist()
+            ventanaResultados = tk.Toplevel(ventana)
+            ventanaResultados.title("Estudiantes de la Sede")
+            tabla = ttk.Treeview(ventanaResultados, columns=["Carnet", "Nombre Completo", "Carrera", "Correo Electrónico"])
+            tabla.heading("#0", text="Carnet")
+            tabla.heading("#1", text="Nombre Completo")
+            tabla.heading("#2", text="Carrera")
+            tabla.heading("#4", text="Correo Electrónico")
+            tabla.pack()
+            admitidosCarreras = {carrera: cantidadAdmitidos(sedeElegida, carrera) for carrera in sedeCarreras}
+            estudiantesGenerados = []
+            mentoresGenerados = []
+            
+            for carrera in sedeCarreras:
+                nombreCarrera = carreras.loc[carrera, sedeElegida]
+                cantidad_estudiantes = admitidosCarreras[carrera]
 
-def asignarMentores():
-    return
+                # Calcular la cantidad de mentores (5% de estudiantes)
+                cantidad_mentores = int(cantidad_estudiantes * 0.05)
 
+                for _ in range(cantidad_estudiantes):
+                    carnet = f"2023{opcionesSedes.index(sedeElegida) + 1:02d}{random.randint(1000, 9999)}"
+                    nombreCompleto = (fake.last_name(), fake.last_name(), fake.first_name())
+                    correo = generarCorreo(nombreCompleto[2], nombreCompleto[0])
+                    estudiantesGenerados.append({'Carnet': carnet, 'Nombre Completo': f"{nombreCompleto[0]} {nombreCompleto[1]} {nombreCompleto[2]}", 'Carrera': nombreCarrera, 'Correo Electrónico': correo})
+
+                # Genera mentores para esta carrera
+                for _ in range(cantidad_mentores):
+                    mentor_carnet = f"2023{opcionesSedes.index(sedeElegida) + 1:02d}{random.randint(1000, 9999)}"
+                    mentor_nombre = (fake.last_name(), fake.last_name(), fake.first_name())
+                    mentor_correo = generarCorreo(mentor_nombre[2], mentor_nombre[0])
+                    mentoresGenerados.append({'Carnet': mentor_carnet, 'Nombre Completo': f"{mentor_nombre[0]} {mentor_nombre[1]} {mentor_nombre[2]}", 'Carrera': nombreCarrera, 'Correo Electrónico': mentor_correo})
+
+            # Agregar estudiantes y mentores a la tabla
+            for estudiante in estudiantesGenerados:
+                tabla.insert("", "end", values=(estudiante['Carnet'], estudiante['Nombre Completo'], estudiante['Carrera'], estudiante['Correo Electrónico']))
+            for mentor in mentoresGenerados:
+                tabla.insert("", "end", values=(mentor['Carnet'], mentor['Nombre Completo'], mentor['Carrera'], mentor['Correo Electrónico']))
+
+            return estudiantesGenerados  # Devuelve la lista de estudiantes generados
+        else:
+            messagebox.showerror("Error", f"La sede '{sedeElegida}' no se encuentra en el archivo Excel.")
+
+    ventanaSede = tk.Toplevel(ventana)
+    ventanaSede.title("Seleccionar Sede")
+    etiquetaSeleccionSede = tk.Label(ventanaSede, text="Selecciona una sede:")
+    etiquetaSeleccionSede.pack()
+    opcionesSedes = ["CAMPUS TECNOLÓGICO LOCAL SAN CARLOS", "CAMPUS TECNOLÓGICO LOCAL SAN JOSÉ", "CENTRO ACADÉMICO DE LIMÓN", "CAMPUS TECNOLÓGICO CENTRAL CARTAGO", "CENTRO ACADÉMICO DE ALAJUELA"]
+    sedeSeleccionada = tk.StringVar()
+    sedeSeleccionada.set(opcionesSedes[0])
+    listaSede = ttk.Combobox(ventanaSede, textvariable=sedeSeleccionada, values=opcionesSedes)
+    listaSede.config(width=42)
+    listaSede.pack()
+    botonConfirmarSede = tk.Button(ventanaSede, text="Mostrar Estudiantes", command=mostrarSede)
+    botonConfirmarSede.pack()
+
+
+
+def asignarMentor():
+    return
 def actualizarEstudiante():
     return
 
@@ -139,7 +197,7 @@ def crearBaseDatos():
     return
 
 def enviarCorreo():
-    return
+    return  
 
 #Interfaz gráfica
 ventana = tk.Tk()
@@ -147,8 +205,8 @@ ventana.title("Atención a la Generación 2024")
 ventana.attributes('-fullscreen', True)
 boton1 = tk.Button(ventana, text="Estudiantes por sede", command=estudiantesSede)
 boton2 = tk.Button(ventana, text="Estudiantes de carrera por sede", command=estudiantesCarrera)
-boton3 = tk.Button(ventana, text="Crear mentores", command=crearMentores, state="disabled")
-boton4 = tk.Button(ventana, text="Asignar mentores", command=asignarMentores, state="disabled")
+boton3 = tk.Button(ventana, text="Crear mentores", command=crearMentores)
+boton4 = tk.Button(ventana, text="Asignar mentores", command=asignarMentor)
 boton5 = tk.Button(ventana, text="Actualizar estudiante", command=actualizarEstudiante, state="disabled")
 boton6 = tk.Button(ventana, text="Generar reportes", command=generarReportes)
 boton7 = tk.Button(ventana, text="Crear base de datos en Excel", command=crearBaseDatos)
